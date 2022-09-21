5fd8a8bf98ae0f817d53869091593ca8a7d437fe
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-21 09:06:12 +0200

    koniec lekcji

---
+++ Pierwszy projekt/FisrstWinFormsApp/MainForm1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace FisrstWinFormsApp
+{
+    partial class MainForm1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 112);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Przycisk";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 524);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm1";
+            this.Text = "Pierwszy program okienkowy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
